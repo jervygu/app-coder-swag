39cb2d5cc5d3dc2250a7fb2364bec84b71d3298c
Jeff Umandap <jeffumandap@Jeffs-MacBook-Pro.local>
2021-03-09 16:06:12 +0800

    Collection views (grid layouts) completed.

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Jeff Umandap on 3/9/21.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImg : UIImageView!
+    @IBOutlet weak var productTitle : UILabel!
+    @IBOutlet weak var productPrice : UILabel!
+    
+    
+}
